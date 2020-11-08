59942ab188b822a9e985392f41bd86d4656d4b2a
Arpita Saha <arpita.sahaa@gmail.com>
2020-11-08 11:13:40 -0500

    added validations and comments

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Using inquirer to gather information about the development team members,
+// and to created objects for each team member as per the respective class
 
 const teamArray = [];
 let id = 1;
+
 const getEmployeeDetails = (questions, flag) => {
   inquirer.prompt(questions).then((answer) => {
-    // create manager object using constructor and push in the team array
     if (flag === "m") {
+      // create manager object using constructor and push in the team array if flag is "m"
       let manager = new Manager(
         answer.employeeName,
         id++,
@@ -28,6 +29,7 @@ const getEmployeeDetails = (questions, flag) => {
       );
       teamArray.push(manager);
     } else if (flag === "i") {
+      //create intern object using constructor and push in the team array if flag is "i"
       let intern = new Intern(
         answer.employeeName,
         id++,
@@ -36,6 +38,7 @@ const getEmployeeDetails = (questions, flag) => {
       );
       teamArray.push(intern);
     } else if (flag === "e") {
+      // create Engineer object using constructor and push in the team array if flag is "e"
       let engineer = new Engineer(
         answer.employeeName,
         id++,
@@ -44,41 +47,25 @@ const getEmployeeDetails = (questions, flag) => {
       );
       teamArray.push(engineer);
     }
+    //based on what type of team member user chooses,
+    //getEmployeeDetails function is called with respective flag and questions
     if (answer.temMemType === "Intern") {
       getEmployeeDetails(internQues, "i");
     } else if (answer.temMemType === "Engineer") {
       getEmployeeDetails(engineerQues, "e");
     } else {
+      // if the user choose None as the next team member then render function is called
+      // with teamArray as the parameter.
+      // This function returns the html data based on the teamArray.
       const renderResp = render(teamArray);
+
+      //write the html data in a file.
       fs.writeFile(outputPath, renderResp, (err) => {
         err ? console.error(err) : console.log("success");
       });
     }
   });
 };
+// call "getEmployeeDetails" function with "managerQues" & "m" as parameters.
 
 getEmployeeDetails(managerQues, "m");
-
-// call "getEmployeeDetails" for "manager question" and create manager object then push it to team array.
-
-// getEmployeeDetails(internQues);
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
